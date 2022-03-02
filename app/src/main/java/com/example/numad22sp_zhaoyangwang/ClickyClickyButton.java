3138502151496c383f5b6b085858c6a828ce7aee
zhaoywang <zhaoywang99@gmail.com>
2022-03-01 23:25:01 -0800

    A3 Clicky Clicky Commit 2

--- app/src/main/java/com/example/numad22sp_zhaoyangwang/ClickyClickyButton.java
+++ app/src/main/java/com/example/numad22sp_zhaoyangwang/ClickyClickyButton.java
@@ -2,13 +2,82 @@ package com.example.numad22sp_zhaoyangwang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ClickyClickyButton extends AppCompatActivity {
 
+    private Button buttonA, buttonB, buttonC, buttonD, buttonE, buttonF;
+    private TextView textView;
+    private int screenDirection = 1; // Whether the current screen is LANDSCAPE/ PORTRAIT
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.clicky_clicky_button);
+        textView = (TextView) findViewById(R.id.CCTextView);
+
+        Button buttonA = (Button) findViewById(R.id.CCButtonA);
+        Button buttonB = (Button) findViewById(R.id.CCButtonB);
+        Button buttonC = (Button) findViewById(R.id.CCButtonC);
+        Button buttonD = (Button) findViewById(R.id.CCButtonD);
+        Button buttonE = (Button) findViewById(R.id.CCButtonE);
+        Button buttonF = (Button) findViewById(R.id.CCButtonF);
+
+
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Pressed: A");
+            }
+        });
+
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Pressed: B");
+            }
+        });
+
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Pressed: C");
+            }
+        });
+
+        buttonD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Pressed: D");
+            }
+        });
+
+        buttonE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Pressed: E");
+            }
+        });
+
+        buttonF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Pressed: F");
+            }
+        });
+
+        if (getResources().getConfiguration().orientation != screenDirection) {
+            screenDirection = getResources().getConfiguration().orientation;
+            setDefaultPressed(textView);
+        }
+    }
+
+    public void setDefaultPressed(TextView textView) {
+        textView.setText("Pressed: -");
     }
 }
--- app/src/main/java/com/example/numad22sp_zhaoyangwang/MainActivity.java
+++ app/src/main/java/com/example/numad22sp_zhaoyangwang/MainActivity.java
@@ -6,23 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.numad22sp_zhaoyangwang.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button button1; // About Me
+    private Button button2; // Clicky Clicky
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        button1 = (Button) findViewById(R.id.AboutMe);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openAboutMe();
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.ClickyClicky);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openClickyClicky();
             }
         });
 
@@ -31,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void openAboutMe() {
         Intent intent = new Intent(this, AboutMeButton.class);
+        startActivity(intent);
+    }
+
+    public void openClickyClicky() {
+        Intent intent = new Intent(this, ClickyClickyButton.class);
         startActivity(intent);
     }
 }
